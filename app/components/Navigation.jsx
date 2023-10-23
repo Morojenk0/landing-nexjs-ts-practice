4e4d26bcf02fc0dc45e0325de1aaaa99f503c33e
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-10-23 23:51:54 +0600

    continued work on Navigation

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -7,12 +7,16 @@ import instagram from '../../public/icons/instagram-fill.svg'
 import twitter from '../../public/icons/twitter-fill.svg'
 import youtube from '../../public/icons/youtube-fill.svg'
 import logo from '../../public/icons/logo.svg'
+import menu from '../../public/icons/menu-line.svg'
 
 export default function Navigation() {
 	return (
+		// ? clean up
+		// ? fix leading?
+		// ? fix breakpoints?
 		<nav>
 			<div className="container">
-				<div className=" hidden tablet:flex tablet:justify-between">
+				<div className="hidden lg:flex justify-between">
 					<address>
 						<div className="flex gap-x-[3rem]">
 							<p>
@@ -82,68 +86,83 @@ export default function Navigation() {
 						</div>
 					</ul>
 				</div>
-				<div className=" py-[1rem] pr-[.30rem] pl-[3.25rem]  rounded-[0.3125rem] bg-white">
-					<ul>
-						<div className="relative flex items-center justify-between gap-x-[2.5rem]">
-							<li>
-								<Link
-									className="text-black text-xl uppercase hover:text-blue hover:underline "
-									href="/"
-								>
-									home
-								</Link>
-							</li>
-							<li>
-								<Link
-									className="text-black text-xl uppercase hover:text-blue hover:underline "
-									href="#"
-								>
-									About
-								</Link>
-							</li>
-							<li>
-								<Link
-									className="text-black text-xl uppercase hover:text-blue hover:underline "
-									href="#"
-								>
-									Features
-								</Link>
-							</li>
-							<div className="absolute  mx-auto py-[2rem] px-[1rem] bg-white rounded-[0.3125rem] shadow-box-shadow">
-								<Link href="/">
-									<Image
-										src={logo}
-										alt="logo"
-									/>
-								</Link>
-							</div>
-							<li>
-								<Link
-									className=" text-black text-xl uppercase hover:text-blue hover:underline "
-									href="#"
-								>
-									Screenshot
-								</Link>
-							</li>
-							<li>
-								<Link
-									className="text-black text-xl uppercase hover:text-blue hover:underline "
-									href="/blog"
-								>
-									Blog
-								</Link>
-							</li>
-							<li>
-								<a
-									className="bg-blue py-[.75rem] px-[1rem] rounded-[0.3125rem] text-white text-xl uppercase"
-									href="https://play.google.com/store"
-									target="_blank"
-								>
-									Download
-								</a>
-							</li>
-						</div>
+				<div className="mt-10 relative flex justify-between items-center p-[.30rem] rounded-[0.3125rem] bg-white">
+					<ul className="hidden ml-[2.95rem] lg:flex items-center gap-x-[2.5rem]">
+						<li>
+							<Link
+								className="text-black text-xl uppercase hover:text-blue underline-offset-[3px] hover:underline "
+								href="/"
+							>
+								home
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="text-black text-xl uppercase hover:text-blue underline-offset-[3px] hover:underline "
+								href="#"
+							>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="text-black text-xl uppercase hover:text-blue underline-offset-[3px] hover:underline "
+								href="#"
+							>
+								Features
+							</Link>
+						</li>
 					</ul>
+					<div className="absolute left-0 right-0 mx-auto lg:w-[12.5rem] py-[1.5rem] px-[.7rem] w-[6.25rem] lg:py-[2rem] lg:px-[1rem] bg-white rounded-[0.3125rem] shadow-box-shadow">
+						<Link href="/">
+							<Image
+								className="lg:w-[10.4375rem] lg:h-[1.75rem]"
+								src={logo}
+								alt="logo"
+							/>
+						</Link>
+					</div>
+					<ul className="hidden lg:flex items-center gap-x-[2.5rem]">
+						<li className="">
+							<Link
+								className=" text-black text-xl uppercase hover:text-blue underline-offset-[3px] hover:underline "
+								href="#"
+							>
+								Screenshot
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="text-black text-xl uppercase hover:text-blue underline-offset-[3px]  hover:underline"
+								href="/blog"
+							>
+								Blog
+							</Link>
+						</li>
+						<li className="">
+							<a
+								className="block bg-blue py-[.75rem] px-[1rem] rounded-[0.3125rem] text-white text-xl uppercase transition hover:bg-[#4542F2]"
+								href="https://play.google.com/store"
+								target="_blank"
+							>
+								Download
+							</a>
+						</li>
+					</ul>
+					<div className="lg:hidden">
+						<Image
+							className="ml-[.55rem]"
+							src={menu}
+							alt="menu"
+						/>
+					</div>
+					<a
+						className="lg:hidden bg-blue py-[.75rem] px-[.4rem] rounded-[0.3125rem] text-white text-[.625rem] font-semibold leading-normal uppercase transition hover:bg-[#4542F2]"
+						href="https://play.google.com/store"
+						target="_blank"
+					>
+						Download
+					</a>
 				</div>
 			</div>
 		</nav>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 // import hero from '../public/free_Ui_view-mobile-app-mockup-1.png'
 export default function Home() {
 	return (
-		<main className=" ">
+		<main className="">
 			<h1 className="text-6xl text-white ">Test</h1>
+			<div className="container bg-blue">2121</div>
 		</main>
 	)
 }
