257cff57e297e51e0fd079b29458298f6eecf966
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-10-28 17:58:19 +0600

    reconstruct layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,15 @@
-function page() {
-	return <div>page</div>
-}
+import Navigation from '../components/Navigation'
 
-export default page
+export default function page() {
+	return (
+		<div>
+			<header className="bg-hero-image bg-cover bg-center bg-no-repeat">
+				<Navigation />
+				<h1 className="text-6xl text-white">its Blog Header Title!</h1>
+			</header>
+			<main>
+				<h1 className="text-6xl text-white">its Main Title!</h1>
+			</main>
+		</div>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
+import Navigation from './components/Navigation'
 
 export default function Home() {
 	return (
-		<main className="">
-			<h1 className="text-6xl text-white ">Test</h1>
-			<div className="container bg-blue">2121</div>
-		</main>
+		<div>
+			<header className="bg-hero-image bg-cover bg-center bg-no-repeat">
+				<Navigation />
+				<h1 className="text-6xl text-white">its Header Content!</h1>
+			</header>
+			<main>
+				<h1 className="text-6xl text-white">its Main Title!</h1>
+			</main>
+		</div>
 	)
 }
